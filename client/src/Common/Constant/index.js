024ee676061d3d8bdf09648e4d24101e0198b1d0
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-03-26 16:28:49 +0900

    Etc :  env 설정

--- client/src/Common/Constant/index.js
+++ client/src/Common/Constant/index.js
@@ -1,13 +1,10 @@
-export const SERVER_URL = `http://ec2-3-36-163-212.ap-northeast-2.compute.amazonaws.com:8080`;
+console.log(process.env);
 
-export const DEPLOYMENT_URL = `http://www.cyber-capstone.kro.kr`;
+export const SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
-export const BASIC_API_URL = `/api/v1`;
+export const BASIC_API_URL = process.env.REACT_APP_BASIC_API_URL;
+
+export const ALERT_GET_URL_BASIC = process.env.REACT_APP_ALERT_GET_URL;
 
 export const ALERT_GET_URL = (id) =>
-  `${SERVER_URL}${BASIC_API_URL}/alert-log/get/${id}`;
-
-export const DEPLOYMENT_API_URL = `${DEPLOYMENT_URL}/api`;
-
-export const DEPLOYMENT_ALERT_GET_URl = (id) =>
-  `${DEPLOYMENT_API_URL}/alert-log/get/${id}`;
+    `${SERVER_URL}${BASIC_API_URL}${ALERT_GET_URL_BASIC}/${id}`;
