131ac72eb1f920628f89aa64066e034cb924547f
Hansol Hong <66724340+Noelsky-code@users.noreply.github.com>
2022-05-25 18:30:23 +0900

    close error fix

--- client/src/Common/Hook/useSocket.js
+++ client/src/Common/Hook/useSocket.js
@@ -5,58 +5,62 @@ let setImagePath = 0;
 let curSocketId = 0;
 let setPrevSocketId = 0;
 const openEvnet = () => {
-  if (setImagePath === 0) console.log("id Socket connected");
-  else console.log("img Socket connected");
+    if (setImagePath === 0) console.log("id Socket connected");
+    else console.log("img Socket connected");
 };
 
 const useOnMessageEvent = (evt) => {
-  const { data: message } = evt;
-  if (message / 1) {
-    setPrevSocketId(curSocketId);
-    setSocketId(message);
-  } else {
-    setImagePath(message);
-  }
+    const { data: message } = evt;
+    if (message / 1) {
+        setPrevSocketId(curSocketId);
+        setSocketId(message);
+    } else {
+        setImagePath(message);
+    }
 };
 
 const errorEvent = (err) => {
-  console.log(err);
+    console.log(err);
 };
-const closeEvent = () => {
-  console.log("closed");
-};
-const socketInit = (socket, setId) => {
-  socket.onopen = openEvnet;
-  socket.onmessage = useOnMessageEvent;
-  socket.onerror = errorEvent;
-  socket.onclose = closeEvent;
+
+const socketInit = (socket, url, setSocket) => {
+    socket.onopen = openEvnet;
+    socket.onmessage = useOnMessageEvent;
+    socket.onerror = errorEvent;
+    socket.onclose = () => {
+        init(socket, url, setSocket);
+    };
 };
 
 export const useSocket = (
-  setSocket,
-  curId,
-  setId,
-  setPrevId,
-  setImgPath,
-  url
+    setSocket,
+    curId,
+    setId,
+    setPrevId,
+    setImgPath,
+    url
 ) => {
-  let socket;
-  if (setId) {
-    setSocketId = setId;
-    curSocketId = curId;
-    setPrevSocketId = setPrevId;
-  }
-  if (setImgPath) setImagePath = setImgPath;
-  useEffect(() => {
+    let socket;
+    if (setId) {
+        setSocketId = setId;
+        curSocketId = curId;
+        setPrevSocketId = setPrevId;
+    }
+    if (setImgPath) setImagePath = setImgPath;
+    useEffect(() => {
+        init(socket, url, setSocket);
+    }, []);
+};
+
+const init = (socket, url, setSocket) => {
     socket = new WebSocket(url);
-    socketInit(socket);
+    socketInit(socket, url, setSocket);
     setSocket(socket);
-  }, []);
 };
 
 export const sendMessage = (socket, msg) => {
-  if (socket.readyState === 1) {
-    socket.send(msg);
-    console.log(`sendMessage : ${msg}`);
-  }
+    if (socket.readyState === 1) {
+        socket.send(msg);
+        console.log(`sendMessage : ${msg}`);
+    }
 };
